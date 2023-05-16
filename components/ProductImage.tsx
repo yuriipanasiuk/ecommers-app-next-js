a6f2684ebcf174548be02c3ac171d0923c60dd65
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-05-16 23:33:13 +0300

    create next js app

---
+++ components/ProductImage.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+
+type Props = {
+  product: Product;
+  fill?: boolean;
+};
+
+const ProductImage = ({ product, fill }: Props) => {
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <>
+      {fill ? (
+        <Image
+          src={product.image}
+          alt={product.title}
+          fill
+          className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
+            loading
+              ? "scale-110 blur-2xl grayscale"
+              : "scale-100 blur-0 grayscale-0"
+          }}`}
+          onLoadingComplete={() => setLoading(false)}
+        />
+      ) : (
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={400}
+          height={1000}
+          className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
+            loading
+              ? "scale-110 blur-2xl grayscale"
+              : "scale-100 blur-0 grayscale-0"
+          }}`}
+          onLoadingComplete={() => setLoading(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default ProductImage;
